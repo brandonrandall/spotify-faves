54648bfc4451bb8cc6dde505670a9da2854cfc0c
Brandon Randall <br.mhcexchange@gmail.com>
2017-10-31 05:20:18 -0600

    creates route for welcome index

---
+++ spec/features/user_can_log_in_with_spotify_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'spotify login' do
+  it 'user can login with spotify oauth' do
+#     As a visitor
+      # When I visit "/"
+      visit '/'
+      # And I click on Login (in the navbar)
+      click_on "Login with Spotify"
+      # Then I should be OAuthed in using Spotify
+      # And I should be on "/dashboard"
+      # And I should see "Logged in as <YOUR_USERNAME>" in the navbar
+      # And I should see a link for "Logout" in the navbar
+      # And when I click "Logout"
+      # Then I should be logged out
+  end
+end
