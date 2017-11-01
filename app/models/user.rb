e5c0741a33eb072c5e061e6ecdd8a644ae0b9c98
Brandon Randall <br.mhcexchange@gmail.com>
2017-11-01 08:07:34 -0600

    styles nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
-
-
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.provider      = auth_info["provider"]
       new_user.uid           = auth_info["uid"]
-      # binding.pry
       new_user.oauth_token   = auth_info.credentials.token
       new_user.oauth_refresh = auth_info.credentials.refresh_token
     end
