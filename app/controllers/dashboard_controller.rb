c5708c0bd10a5f1d661f86efb4973ed4645faa39
Brandon Randall <br.mhcexchange@gmail.com>
2017-11-01 07:41:29 -0600

    working functionality for oauth with spotify

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
   def index
     @spotify_connection = Faraday.new(url: "https://api.spotify.com/")
-    response = Faraday.get(url: "/v1/me/top/artists#{ENV["client_id"]}#{ENV["client_secret"]}")
-    parsed = JSON.parse(response.body)
+    # response = Faraday.get(url: "/v1/me/top/artists#{ENV["client_id"]}#{ENV["client_secret"]}")
+    # parsed = JSON.parse(response.body)
     # @top_artists = spotify_user.top_artists
-    top_tracks = ("/v1/me/top/artists")
+    # top_tracks = ("/v1/me/top/artists")
     # spotify_user.top_artists #=> (Artist array)
     album_results = RSpotify::Album.search("The Eminem Show")
     artist = album_results.first.artists.first.name
