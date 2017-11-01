2a5a8d0bb148cf15920b48f7a9b03628db806619
Brandon Randall <br.mhcexchange@gmail.com>
2017-11-01 08:00:39 -0600

    adds nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.provider      = auth_info["provider"]
       new_user.uid           = auth_info["uid"]
+      # binding.pry
       new_user.oauth_token   = auth_info.credentials.token
       new_user.oauth_refresh = auth_info.credentials.refresh_token
     end
--- spec/features/user_can_log_in_with_spotify_spec.rb
+++ spec/features/user_can_log_in_with_spotify_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'spotify login' do
   it 'user can login with spotify oauth' do
     OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+    OmniAuth.config.mock_auth[:spotify] = OmniAuth::AuthHash.new({
       provider: 'spotify',
       uid: "br.mhc",
       credentials:
@@ -20,10 +20,14 @@ describe 'spotify login' do
       # Then I should be OAuthed in using Spotify
       # And I should be on "/dashboard"
       expect(current_path).to eq("/dashboard")
+      brandon = User.last
       # And I should see "Logged in as <YOUR_USERNAME>" in the navbar
-      expect(page).to have_content("Hello, br.mhc")
+      expect(page).to have_content("Get down #{brandon.uid}, wit yo spotify self!")
       # And I should see a link for "Logout" in the navbar
+      click_on "Logout"
       # And when I click "Logout"
+      expect(path).to eq("/root")
+      expect(page).to have_content("Login with Spotify")
       # Then I should be logged out
   end
 end
