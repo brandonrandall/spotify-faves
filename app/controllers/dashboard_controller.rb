758546f3ff701cdab8f2669d5e7d49a580604b89
Brandon Randall <br.mhcexchange@gmail.com>
2017-11-01 07:37:01 -0600

    having trouble with the token getting a 401 stating token expired

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @spotify_connection = Faraday.get(url: "https://api.spotify.com")
+    @spotify_connection = Faraday.new(url: "https://api.spotify.com/")
+    response = Faraday.get(url: "/v1/me/top/artists#{ENV["client_id"]}#{ENV["client_secret"]}")
+    parsed = JSON.parse(response.body)
+    binding.pry
+
     # @top_artists = spotify_user.top_artists
     top_tracks = ("/v1/me/top/artists")
     # spotify_user.follow(playlist)
@@ -16,7 +20,5 @@ class DashboardController < ApplicationController
     # @playlists.push(test_playlist.name)
     # spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
     # @top_ten_artists =
-    @top_artist = Faraday.get(url: "https://api.spotify.com/v1/me/top/artists&client_id=#{}&client_secret=")
-    binding.pry
   end
 end
