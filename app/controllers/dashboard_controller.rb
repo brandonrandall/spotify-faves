a09a764786ba996256723e45a43afbdb9daa10bb
Brandon Randall <br.mhcexchange@gmail.com>
2017-11-01 06:17:16 -0600

    adds parts or required urls

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,19 +1,22 @@
 class DashboardController < ApplicationController
   def index
-    @top_artists = spotify_user.top_artists
+    @spotify_connection = Faraday.get(url: "https://api.spotify.com")
+    # @top_artists = spotify_user.top_artists
+    top_tracks = ("/v1/me/top/artists")
     # spotify_user.follow(playlist)
     # spotify_user.follows?(artists)
     # spotify_user.unfollow(users)
 
     # Get user's top played artists and tracks
     # spotify_user.top_artists #=> (Artist array)
-    @playlists = []
-    test_playlist = spotify_user.create_playlist!('test')
-    playlists.push(test_playlist.name)
-    spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
+    album_results = RSpotify::Album.search("The Eminem Show")
+    artist = album_results.first.artists.first.name
+    # @playlists = []
+    # test_playlist = spotify_user.create_playlist!('test')
+    # @playlists.push(test_playlist.name)
+    # spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
+    # @top_ten_artists =
+    @top_artist = Faraday.get(url: "https://api.spotify.com/v1/me/top/artists&client_id=#{}&client_secret=")
     binding.pry
-
-
-
   end
 end
